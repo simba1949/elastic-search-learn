10ebe602609348db03366c1aa014fc32c4d31da9
simba1949 <simba1949@outlook.com>
2023-09-16 13:40:36 +0800

    feature：使用 elastic-search-rest-client 对索引获取（单个、所有）、删除（单个、正则匹配、所有）

--- elastic-search-rest-client/src/main/java/top/simba1949/restClient/operation/IndexOperation.java
+++ elastic-search-rest-client/src/main/java/top/simba1949/restClient/operation/IndexOperation.java
@@ -1,6 +1,5 @@
 package top.simba1949.restClient.operation;
 
-import com.alibaba.fastjson2.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.Request;
@@ -23,7 +22,12 @@ public class IndexOperation {
 		RestClient restClient = ESClient.initClient();
 
 		// syncPut(restClient);
-		asyncPut(restClient);
+		// asyncPut(restClient);
+		// getIndex(restClient);
+		// getAllIndex(restClient);
+		// deleteIndex(restClient);
+		// deleteMultiIndex(restClient);
+		deleteAllIndex(restClient);
 
 		restClient.close();
 	}
@@ -33,6 +37,10 @@ public class IndexOperation {
 	 * 新增索引
 	 * 同步新增索引：<url href="https://www.elastic.co/guide/en/elasticsearch/client/java-api-client/current/java-rest-low-usage-requests.html"></url>
 	 *
+	 * <p>
+	 * PUT index_user
+	 * </p>
+	 *
 	 * @param restClient
 	 * @throws IOException
 	 */
@@ -41,12 +49,15 @@ public class IndexOperation {
 
 		Response response = restClient.performRequest(request);
 		String entityStr = EntityUtils.toString(response.getEntity());
-		log.info("索引操作：新增索引的响应是：{}", entityStr);
+		log.info("新增索引，响应结果：{}", entityStr);
 	}
 
 	/**
 	 * 新增索引
 	 * 异步新增索引
+	 * <p>
+	 * PUT index_user
+	 * </p>
 	 *
 	 * @param restClient
 	 */
@@ -56,11 +67,9 @@ public class IndexOperation {
 		restClient.performRequestAsync(request, new ResponseListener() {
 			@Override
 			public void onSuccess(Response response) {
-				log.info("索引操作：新增索引的成功：{}", JSONObject.toJSONString(response));
-
 				try {
 					String entityStr = EntityUtils.toString(response.getEntity());
-					log.info("索引操作：新增索引的响应是：{}", entityStr);
+					log.info("新增索引，响应结果：{}", entityStr);
 				} catch (IOException e) {
 					throw new RuntimeException(e);
 				}
@@ -68,11 +77,112 @@ public class IndexOperation {
 
 			@Override
 			public void onFailure(Exception e) {
-				log.info("索引操作：新增索引的失败，异常信息：{}", e.getMessage(), e);
+				log.info("新增索引的失败，异常信息：{}", e.getMessage(), e);
 			}
 		});
 
 		// 防止进程结束，无法收到响应结果
 		new CountDownLatch(1).wait();
+	}
+
+	/**
+	 * 获取单个索引
+	 * <p>
+	 * GET index_user
+	 * </p>
+	 *
+	 * @param restClient
+	 */
+	public static void getIndex(RestClient restClient) throws IOException {
+		Request request = new Request("GET", "index_user");
+
+		Response response = restClient.performRequest(request);
+		String entityStr = EntityUtils.toString(response.getEntity());
+		log.info("index_user 索引信息，响应结果： {}", entityStr);
+	}
+
+	/**
+	 * 获取所有索引信息
+	 *
+	 * <p>
+	 * GET _cat/indices?v=true&pretty
+	 * </p>
+	 *
+	 * @param restClient
+	 * @throws IOException
+	 */
+	public static void getAllIndex(RestClient restClient) throws IOException {
+		Request request = new Request("GET", "_cat/indices?v=true&pretty");
+		Response response = restClient.performRequest(request);
+		String entityStr = EntityUtils.toString(response.getEntity());
+		log.info("获取所有索引信息，响应结果：{}", entityStr);
+	}
+
+	/**
+	 * 删除单个索引
+	 * <p>
+	 * DELETE index_user
+	 * </p>
+	 *
+	 * @param restClient
+	 * @throws IOException
+	 */
+	public static void deleteIndex(RestClient restClient) throws IOException {
+		Request request = new Request("DELETE", "index_user");
+		Response response = restClient.performRequest(request);
+		String entityStr = EntityUtils.toString(response.getEntity());
+		log.info("删除单个索引，响应结果：{}", entityStr);
+	}
+
+	/**
+	 * 删除多个索引（有一个不存在，则不会删除）
+	 * <p>
+	 * DELETE role_index,permission_index
+	 * </p>
+	 *
+	 * @param restClient
+	 * @throws IOException
+	 */
+	public static void deleteMultiIndex(RestClient restClient) throws IOException {
+		Request request = new Request("DELETE", "index_role, index_permission");
+		Response response = restClient.performRequest(request);
+		String entityStr = EntityUtils.toString(response.getEntity());
+		log.info("删除多个索引，响应结果：{}", entityStr);
+	}
+
+	/**
+	 * 删除匹配的索引
+	 * <p>
+	 * DELETE index_*
+	 * </p>
+	 * <p>
+	 * 如果出现【Wildcard expressions or all indices are not allowed】，
+	 * 需要将 elasticsearch.yml 中 action.destructive_requires_name 设置为 false 并重启 ElasticSearch.
+	 * </p>
+	 *
+	 * @param restClient
+	 * @throws IOException
+	 */
+	public static void deleteRegexIndex(RestClient restClient) throws IOException {
+		Request request = new Request("DELETE", "index_*");
+		Response response = restClient.performRequest(request);
+		String entityStr = EntityUtils.toString(response.getEntity());
+		log.info("删除正则表达式匹配的索引，响应结果：{}", entityStr);
+	}
+
+	/**
+	 * 删除所有的索引
+	 * <p>
+	 * DELETE _all
+	 * </p>
+	 *
+	 * @param restClient
+	 * @throws IOException
+	 */
+	public static void deleteAllIndex(RestClient restClient) throws IOException {
+		Request request = new Request("DELETE", "_all");
+		Response response = restClient.performRequest(request);
+		String entityStr = EntityUtils.toString(response.getEntity());
+		log.info("删除所有索引，响应结果：{}", entityStr);
 	}
 }
